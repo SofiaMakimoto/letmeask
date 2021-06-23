0958dfad7704af2695ee572fe0ecb848a6112a8d
Sofia Makimoto <sofiamakimoto@gmail.com>
2021-06-23 15:56:01 -0300

    class 2 - firebase google auth

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,8 +1,19 @@
+import { createContext } from 'react';
+import {  BrowserRouter, Route } from 'react-router-dom'
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+export const TestContext = createContext('');
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <TestContext.Provider value={'Teste'}>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewRoom}/>
+      </TestContext.Provider>
+    </BrowserRouter>
   )
 }
 
